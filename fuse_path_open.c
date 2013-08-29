c85ac0cd72e004e76c871a0326a34bd78b3a78d4
Elod Csirmaz <csirmazelodpal@gmail.com>
2013-08-29 23:49:46 +0100

    Improvements

--- fuse_path_open.c
+++ fuse_path_open.c
@@ -106,14 +106,22 @@ int $open(const char *path, struct fuse_file_info *fi)
          }
          mfd->mainfd = fd;
 
-         // We need to store the inode no of the file.
-         // We can only do that here after the open as it's possibly new
-         // TODO but if it's not, we could get the inode from $n_open!
-         if(fstat(fd, &mystat) == -1){
-            waserror = errno;
-            break;
+         do{
+
+            // We need to store the inode no of the file.
+            // We can only do that here after the open as it's possibly new
+            // TODO but if it's not, we could get the inode from $n_open!
+            if(fstat(fd, &mystat) == -1){
+               waserror = errno;
+               break;
+            }
+            mfd->main_inode = mystat.st_ino;
+
+         }while(0);
+
+         if(waserror != 0){
+            close(fd);
          }
-         mfd->main_inode = mystat.st_ino;
 
       }while(0);
 
@@ -183,12 +191,20 @@ int $create(const char *path, mode_t mode, struct fuse_file_info *fi)
          }
          mfd->mainfd = fd;
 
-         // We need to store the inode no of the file. We can only do that here as it's new
-         if(fstat(fd, &mystat) == -1){
-            waserror = errno;
-            break;
+         do{
+
+            // We need to store the inode no of the file. We can only do that here as it's new
+            if(fstat(fd, &mystat) == -1){
+               waserror = errno;
+               break;
+            }
+            mfd->main_inode = mystat.st_ino;
+
+         }while(0);
+
+         if(waserror != 0){
+            close(fd);
          }
-         mfd->main_inode = mystat.st_ino;
 
       }while(0);
 
@@ -216,46 +232,50 @@ int $create(const char *path, mode_t mode, struct fuse_file_info *fi)
 //   int (*truncate) (const char *, off_t);
 int $truncate(const char *path, off_t newsize)
 {
-   struct $fd_t mfd;
+   struct $fd_t myfd;
+   struct $fd_t *mfd;
    struct stat mystat;
    int ret;
+   int fd;
    int waserror = 0;
 
    $$IF_PATH_MAIN_ONLY
+
+   mfd = &myfd;
 
    log_msg("  trunc(path=\"%s\" size=%zu)\n", path, newsize);
 
    // TODO should there be a lock for the whole of truncate?
 
-   if(unlikely((ret = $n_open(&(mfd), path, fpath, fsdata)) != 0)){
+   if(unlikely((ret = $n_open(mfd, path, fpath, fsdata)) != 0)){
       return ret;
    }
 
    // TODO Return here if the file does not exist.
 
    do{
-      ret = open(fpath, O_RDONLY);
-      if(ret == -1){
+      fd = open(fpath, O_RDONLY);
+      if(fd == -1){
          waserror = errno;
          $dlogdbg("truncate(%s): failed to open main file err %d = %s\n", fpath, waserror, strerror(waserror));
          break;
       }
-      mfd.mainfd = ret;
+      mfd->mainfd = fd;
 
       do{
 
          // Put the inode into mfd
-         if(fstat(mfd.mainfd, &mystat) != 0){
+         if(fstat(fd, &mystat) != 0){
             waserror = errno;
             $dlogdbg("truncate(%s): failed to stat main file err %d = %s\n", fpath, waserror, strerror(waserror));
             break;
          }
-         mfd.main_inode = mystat.st_ino;
+         mfd->main_inode = mystat.st_ino;
 
          // If the file existed and was larger than newsize, save the blocks
          // NB Any blocks outside the current main file should have already been saved
-         if(mfd.mapheader.exists == 1 && newsize < mfd.mapheader.fstat.st_size){
-            ret = $b_write(fsdata, &(mfd), mfd.mapheader.fstat.st_size - newsize, newsize);
+         if(mfd->mapheader.exists == 1 && newsize < mfd->mapheader.fstat.st_size){
+            ret = $b_write(fsdata, mfd, mfd->mapheader.fstat.st_size - newsize, newsize);
             if(ret != 0){
                waserror = -ret;
                $dlogdbg("truncate(%s): b_write failed err %d = %s\n", fpath, waserror, strerror(waserror));
@@ -263,25 +283,30 @@ int $truncate(const char *path, off_t newsize)
             }
          }
 
-         if(truncate(fpath, newsize) != 0){
-            waserror = errno;
-            $dlogdbg("truncate(%s): truncate failed err %d = %s\n", fpath, waserror, strerror(waserror));
-            break;
-         }
-
       }while(0);
 
-      close(mfd.mainfd);
+      close(fd);
 
    }while(0);
 
    // TODO CLEAN UP MAP/DAT FILES
-   if(unlikely((ret = $n_close(&(mfd))) != 0)){
+   if(unlikely((ret = $n_close(mfd)) != 0)){
       $dlogdbg("truncate(%s): n_close failed err %d = %s\n", fpath, -ret, strerror(-ret));
       return ret;
    }
 
-   return -waserror;
+   if(waserror != 0){
+      return -waserror;
+   }
+
+   // Actually do the truncate
+   if(truncate(fpath, newsize) != 0){
+      waserror = errno;
+      $dlogdbg("truncate(%s): truncate failed err %d = %s\n", fpath, waserror, strerror(waserror));
+      return -errno;
+   }
+
+   return 0;
 
    // TODO add optimisation: copy file to snapshot if that's faster?
    // TODO add optimisation: set whole_saved if newsize==0
