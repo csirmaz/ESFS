6a062046b39a105dbcc1cc144840f25c39dad8b1
Elod Csirmaz <csirmazelodpal@gmail.com>
2013-10-10 00:18:56 +0100

    Improved logging

--- compile.pl
+++ compile.pl
@@ -2,10 +2,17 @@
 
 while(<STDIN>){
    s/`/`x/g;
+
+   # Automatically add file name and line number to log lines
+   s/\\\"/`q/g;
+   s/\$dlog([a-z]+)\("([^"]*?)"/\$dlog$1("\%s(\%d): $2",__FILE__,__LINE__/g;
+   s/`q/\\"/g;
+
    s/\\\$/`S/g;
    s/\$\$/ESFS_/g;
    s/\$/esfs_/g;
    s/`S/\$/g;
+
    s/`x/`/g;
    print;
 }
